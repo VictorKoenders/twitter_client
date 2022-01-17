0c301260cebc5f0ce6c9cba50023aa401443e28d
Victor Koenders <gpg@trangar.com>
2022-01-17 11:11:11 +0100

    Started working on generating release binaries on tags

---
+++ build.rs
@@ -0,0 +1,30 @@
+use std::io::Write;
+use std::{env, fs::File, path::Path};
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    let _ = dotenv::dotenv();
+    println!("cargo:rerun-if-changed=.env");
+
+    let twitter_id = env::var("TWITTER_CLIENT_ID").expect("Missing env variable TWITTER_CLIENT_ID");
+    let twitter_secret =
+        env::var("TWITTER_CLIENT_SECRET").expect("Missing env variable TWITTER_CLIENT_SECRET");
+    println!("cargo:rerun-if-env-changed=TWITTER_CLIENT_ID");
+    println!("cargo:rerun-if-env-changed=TWITTER_CLIENT_SECRET");
+
+    let dest_path = Path::new(&out_dir).join("twitter_credentials.rs");
+    let mut file = File::create(dest_path).unwrap();
+    writeln!(
+        &mut file,
+        "const fn twitter_id() -> &'static str {{ {:?} }}",
+        twitter_id
+    )
+    .unwrap();
+    writeln!(
+        &mut file,
+        "const fn twitter_secret() -> &'static str {{ {:?} }}",
+        twitter_secret
+    )
+    .unwrap();
+}
